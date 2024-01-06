32380b4ab1ad84c9ce4792ceca8441dd31ce0800
d3ffy <tv4570@gmail.com>
2024-01-06 23:35:56 +0700

    Update MenuPage.kt

--- app/src/main/java/com/example/jsmt_game/page/MenuPage.kt
+++ app/src/main/java/com/example/jsmt_game/page/MenuPage.kt
@@ -1,10 +1,13 @@
 package com.example.jsmt_game.page
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -13,43 +16,45 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.jsmt_game.ui.theme.JSMT_GameTheme
 
 @Composable
 fun MenuPage(navController: NavHostController) {
 
     Column(
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .background(Color.LightGray)
+            .fillMaxSize()
+            .background(Color.White)
     ) {
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "Select Level",
-            color = Color.White,
+            color = Color.Black,
             fontSize = 40.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 30.dp)
         )
-        Button(onClick = {
-            navController.navigate("first_Screen")
-        }) {
-            Text(text = "Back to title")
-        }
         val numbers = (1..30).toList()
-
         Column(
             modifier = Modifier
-                .background(Color.White),
+                .background(Color.White)
+                .padding(16.dp)
+
         ) {
-            // Split the list into chunks of 5
             val chunkedNumbers = numbers.chunked(5)
 
             for (chunk in chunkedNumbers) {
+                Spacer(modifier = Modifier.height(8.dp))
                 Row(
                     modifier = Modifier
-                    ,
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
                 ) {
                     for (number in chunk) {
                         Button(
@@ -59,14 +64,33 @@ fun MenuPage(navController: NavHostController) {
                             modifier = Modifier
                                 .weight(1f)
                                 .fillMaxWidth()
-                                .padding(top = 8.dp, bottom = 8.dp) //
+                                .padding(top = 8.dp, bottom = 8.dp)
+                                .background(Color.White)
+                                .clickable {
+                                    navController.navigate("Screen$number")
+                                }
                         ) {
                             Text(text = String.format("%d", number))
                         }
+
                     }
                 }
             }
         }
+        Button(
+            onClick = {
+                navController.navigate("title_Screen")
+            }
+        ) {
+            Text(text = "Back to Title")
+        }
+    }
+}
 
+@Preview
+@Composable
+fun Preview() {
+    JSMT_GameTheme {
+        MenuPage(navController = rememberNavController())
     }
 }
